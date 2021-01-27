28cdae08e9ad0870afe29adc56f30400297d88f0
Andrew Nowak <anowak4320@stu.d214.org>
2021-01-27 14:34:07 -0600

    Initial Commit

---
+++ Labyrinth Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Labyrinth Project
+//
+//  Created by Andrew Nowak on 1/27/21.
+//  Copyright © 2021 Andrew Nowak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Labyrinth ProjectTests/Labyrinth_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  Labyrinth_ProjectTests.swift
+//  Labyrinth ProjectTests
+//
+//  Created by Andrew Nowak on 1/27/21.
+//  Copyright © 2021 Andrew Nowak. All rights reserved.
+//
+
+import XCTest
+@testable import Labyrinth_Project
+
+class Labyrinth_ProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
