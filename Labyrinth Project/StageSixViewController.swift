4f3410a96798766dcd99a007512010f20098f084
anowak-hub <anowak4320@stu.d214.org>
2021-02-24 20:30:29 -0500

    Modified Stage 4. Finalized Stage 5. Created and completed Stage 6. Created a UIViewController for Stage 7.

---
+++ Labyrinth Project/StageSixViewController.swift
@@ -0,0 +1,26 @@
+//
+//  StageSixViewController.swift
+//  Labyrinth Project
+//
+//  Created by user173487 on 2/24/21.
+//  Copyright © 2021 Andrew Nowak. All rights reserved.
+//
+
+import UIKit
+
+class StageSixViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func whenGestureSwiped(_ sender: UISwipeGestureRecognizer) {
+        print("Swipe Gesture Recognized")
+    
+    }
+    
+    
+
+}
