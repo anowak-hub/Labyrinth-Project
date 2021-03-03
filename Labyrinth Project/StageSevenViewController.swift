70d2c5f408cb67be5b4c6cfd7c25ca525139c35e
Andrew Nowak <anowak4320@stu.d214.org>
2021-03-03 14:54:44 -0600

    Began planning and working on Stage 7.

---
+++ Labyrinth Project/StageSevenViewController.swift
@@ -0,0 +1,34 @@
+//
+//  StageSevenViewController.swift
+//  Labyrinth Project
+//
+//  Created by Andrew Nowak on 3/3/21.
+//  Copyright © 2021 Andrew Nowak. All rights reserved.
+//
+
+import UIKit
+
+import SafariServices
+
+class StageSevenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+        
+        
+        //MARK: - Notes/Criteria
+        
+        // This stage must use the Safari web browser to find out the solution to advance in the game
+        // Make this stage have at least 4 buttons as answer choices
+    
+    
+    }
+
+
+
+}
+    
