533b95f3e8b913c1e77b2c76924e6827dd9df62f
anowak-hub <anowak4320@stu.d214.org>
2021-03-04 21:18:25 -0500

    Completed Stage 7

--- Labyrinth Project/StageSevenViewController.swift
+++ Labyrinth Project/StageSevenViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SafariServices
 
 class StageSevenViewController: UIViewController {
-    let answerURL = URL(string: "https://en.wikipedia.org/wiki/List_of_most-disliked_YouTube_videos")
+    let answerURL = URL(string: "https://www.theverge.com/2018/12/13/18137894/youtube-rewind-2018-dislike-shane-dawson-logan-paul-pewdiepie-mkbhd-philip-defranco")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +24,7 @@ class StageSevenViewController: UIViewController {
         //MARK: - Notes/Criteria
         
         // This stage must use the Safari web browser to find out the solution to advance in the game
-        // Make this stage have at least 4 buttons as answer choices
+        // Have this stage be a multiple choice response
     
         
     }
